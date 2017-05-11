5eb4668a5107e79378f4e65749eb78e86e9b6048
Leslie <leslie@nearbuysolutions.com>
2017-05-11 17:46:52 +0400

    New elements for the recipe item + header working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 
@@ -8,8 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-  	new Recipe('A test recipe', 'This is a simply a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  	new Recipe('A test recipe 2', 'This is a simply a test!!!', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+  	new Recipe('A test recipe', 'This is a simply a test', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRUCjnRYlj2NiUeoYhRBiHOq7NTJ7aJ9jvT0WrjSvxR0fiN1x2z')
   ];
 
   constructor() { }
@@ -17,4 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
